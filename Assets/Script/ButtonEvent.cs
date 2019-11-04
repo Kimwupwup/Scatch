f72e4a3f67e528a413b5fb1a4323577dc4f7f1d1
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-04 21:50:14 +0900

    Next Button + scratch

--- Assets/Script/ButtonEvent.cs
+++ Assets/Script/ButtonEvent.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonEvent : MonoBehaviour
 {
     public void Reset()
     {
+        Compiler compiler = GameObject.FindGameObjectWithTag("compiler").GetComponent<Compiler>();
+        compiler.ResetView();
+        Time.timeScale = 1;
+    }
+
+    public void NextStage()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int curSecene = scene.buildIndex;
+        int nextScene = curSecene + 1;
+        SceneManager.LoadScene(nextScene);
         Compiler compiler = GameObject.FindGameObjectWithTag("compiler").GetComponent<Compiler>();
         compiler.ResetView();
         Time.timeScale = 1;
