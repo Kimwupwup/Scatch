f19ecdcf7a393697483de113d99d980ba73072f4
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-11-05 00:35:45 +0900

    Stage map gu hyun

---
+++ Assets/Script/StageButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageButton : MonoBehaviour
+{
+    private string btnString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnString = this.name;
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(btnString);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
