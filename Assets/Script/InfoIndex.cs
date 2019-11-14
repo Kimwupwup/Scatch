e296d6b20e64da4e3b31f16254ce77698a276f23
Wuppu <wndud1640@gmail.com>
2019-11-14 15:58:03 +0900

    UI명암 추가 PREV버튼 추가 Info창 크기 및 위치 수정 info 페이지 넘버 추가

--- Assets/Script/InfoIndex.cs
+++ Assets/Script/InfoIndex.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoIndex : MonoBehaviour
 {
     private int allContext;
     private int curContext;
+    public Text pageNumber;
+
     // Start is called before the first frame update
     void Start()
     {
         curContext = 0;
-        allContext = gameObject.transform.childCount - 2;  //0~curContext-1까지 접근하면 됌.
+        allContext = gameObject.transform.childCount - 3;  //0~curContext-1까지 접근하면 됌.
+        pageNumber.text = $"{curContext + 1}/{allContext}";
     }
 
     // Update is called once per frame
@@ -24,6 +28,7 @@ public class InfoIndex : MonoBehaviour
         gameObject.transform.GetChild(curContext).gameObject.SetActive(false);
         curContext = (curContext + 1) % allContext;
         gameObject.transform.GetChild(curContext).gameObject.SetActive(true);
+        pageNumber.text = $"{curContext + 1}/{allContext}";
     }
 
     public void GoPrevPage()
@@ -32,5 +37,6 @@ public class InfoIndex : MonoBehaviour
         curContext -= 1;
         if (curContext < 0) curContext = allContext - 1;
         gameObject.transform.GetChild(curContext).gameObject.SetActive(true);
+        pageNumber.text = $"{curContext + 1}/{allContext}";
     }
 }
