194ff59cc657615fab8f0fa1429fcf17ceab14cc
Wuppu <wndud1640@gmail.com>
2019-11-14 19:39:41 +0900

    sub flag, goal flag의 슬라이드 크기를 작게 조정 튜토리얼은 저장되지 않도록 코드 수정 및 스크립트 제거 튜토리얼 작업 중...

---
+++ Assets/Script/Tutorial/PlayerChat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerChat : MonoBehaviour
+{
+    private Vector2 headPos;
+    public GameObject dialogPanel;
+    private List<string> dialog = new List<string>();
+    private int currentDialog = 0;
+    private Text content;
+
+    void Start() {
+        content = dialogPanel.transform.GetChild(0).GetComponent<Text>();
+        headPos = this.transform.position;
+        headPos.y += 1f;
+        dialogPanel.transform.position = headPos;
+      
+        dialog.Add("안녕하세요! 저는 너구리가 아니라 고양이입니다!");
+        dialog.Add("앞에 있는 깃발에 가면 다음 스테이지로 갈 수 있어요!");
+        dialog.Add("좌측 상단에 있는 메뉴버튼을 눌러 [MOVE] 버튼을 밑에 CODE창에 올려주세요!");
+        dialog.Add("[MOVE] 는 한개당 무조건 앞으로 한칸 이동해요!");
+        dialog.Add("[MOVE] 를 올리고 중앙 상단에 있는 OFF토글을 누르면 실행됩니다!");
+
+        content.text = dialog[currentDialog];
+    }
+
+    public void NextDialog()
+    {
+        if (currentDialog < dialog.Count - 1)
+            content.text = dialog[++currentDialog];
+        else
+            dialogPanel.SetActive(false);
+    }
+}
