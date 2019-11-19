6789ee6264ed73a3883ccf136c3ea27c422b068c
Wuppu <wndud1640@gmail.com>
2019-11-19 11:06:05 +0900

    스테이지 타일 component옵션 변경: 콜라이더를 폴리곤 타입으로 변경 BGM 볼륨 뮤트 기능 구현

--- Assets/Script/BGMController.cs
+++ Assets/Script/BGMController.cs
@@ -6,17 +6,37 @@ using UnityEngine.UI;
 public class BGMController : MonoBehaviour
 {
     private AudioSource bgm;
-    private Slider slider;
-
+    private Slider sliderVolume;
+    private Toggle toggleMute;
     // Start is called before the first frame update
     void Start()
     {
         bgm = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>();
-        slider = transform.GetComponent<Slider>();
-        slider.value = bgm.volume;
+        sliderVolume = transform.GetChild(0).GetComponent<Slider>();
+        toggleMute = transform.GetChild(1).GetComponent<Toggle>();
+        sliderVolume.onValueChanged.AddListener(VolumeChanged);
+        toggleMute.onValueChanged.AddListener(VolumeMute);
+        sliderVolume.value = bgm.volume;
+        toggleMute.isOn = bgm.mute;
+        if (toggleMute.isOn)
+            sliderVolume.enabled = false;
+        else
+            sliderVolume.enabled = true;
     }
 
-    public void VolumeChanged() {
-        bgm.volume = slider.value;
+    public void VolumeChanged(float f) {
+        bgm.volume = f;
+    }
+
+    public void VolumeMute(bool b) {
+        bgm.mute = b;
+        if (b) {
+            sliderVolume.fillRect.gameObject.GetComponent<Image>().color = Color.gray;
+            sliderVolume.enabled = false;
+        }            
+        else {
+            sliderVolume.fillRect.gameObject.GetComponent<Image>().color = Color.green;
+            sliderVolume.enabled = true;
+        }
     }
 }
