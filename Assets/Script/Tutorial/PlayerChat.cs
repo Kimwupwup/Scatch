2796f4949fd1c410003d4992138ebc1a434be178
Wuppu <wndud1640@gmail.com>
2019-11-19 16:02:39 +0900

    대사 수정

--- Assets/Script/Tutorial/PlayerChat.cs
+++ Assets/Script/Tutorial/PlayerChat.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerChat : MonoBehaviour
-{
+public class PlayerChat : MonoBehaviour {
     private Vector2 headPos;
     public GameObject dialogPanel;
     private List<string> dialog = new List<string>();
@@ -16,18 +15,17 @@ public class PlayerChat : MonoBehaviour
         headPos = this.transform.position;
         headPos.y += 1f;
         dialogPanel.transform.position = headPos;
-      
-        dialog.Add("안녕하세요! 저는 너구리입니다!");
+
+        dialog.Add("안녕하세요! 저는 널구리입니다!");
         dialog.Add("앞에 있는 깃발에 가면 다음 스테이지로 갈 수 있어요!");
-        dialog.Add("좌측 상단에 있는 메뉴버튼을 눌러 [MOVE] 버튼을 밑에 CODE창에 올려주세요!");
-        dialog.Add("[MOVE] 는 한개당 무조건 앞으로 한칸 이동해요!");
+        dialog.Add("좌측 상단에 있는 메뉴 버튼을 눌러 [MOVE] 버튼을 밑 CODE창에 올려주세요!");
+        dialog.Add("[MOVE] 는 한 블럭당 앞으로 한 칸 이동해요!");
         dialog.Add("[MOVE] 를 올리고 중앙 상단에 있는 OFF토글을 누르면 실행됩니다!");
 
         content.text = dialog[currentDialog];
     }
 
-    public void NextDialog()
-    {
+    public void NextDialog() {
         if (currentDialog < dialog.Count - 1)
             content.text = dialog[++currentDialog];
         else
--- Assets/Script/Tutorial/PlayerChat1.cs
+++ Assets/Script/Tutorial/PlayerChat1.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerChat1 : MonoBehaviour
-{
+public class PlayerChat1 : MonoBehaviour {
     // Start is called before the first frame update
     private Vector2 headPos;
     public GameObject dialogPanel;
@@ -17,19 +16,18 @@ public class PlayerChat1 : MonoBehaviour
         headPos = this.transform.position;
         headPos.y += 1f;
         dialogPanel.transform.position = headPos;
-      
-        dialog.Add("앞에 계단이 한개 있어요!");
+
+        dialog.Add("앞에 계단이 있어요!");
         dialog.Add("계단을 오르려면 점프해서 앞으로 가야겠죠?");
-        dialog.Add("[JUMP] 블럭을 사용하면 한칸 위로 점프할 수 있어요!");
-        dialog.Add("좌측 상단에 있는 메뉴버튼을 눌러 위에 보이는 코드와 같이 만들어주세요!");
-        dialog.Add("블럭 아래에 다른 블럭을 올리면 자동으로 연결되요!");
+        dialog.Add("[JUMP] 블럭을 사용하면 한 칸 위로 점프할 수 있어요!");
+        dialog.Add("좌측 상단에 있는 메뉴 버튼을 눌러 위에 보이는 코드와 같이 만들어주세요!");
+        dialog.Add("블럭 아래에 다른 블럭을 올리면 자동으로 연결돼요!");
         dialog.Add("블럭을 만들었나요? 실행해서 확인해봐요!");
 
         content.text = dialog[currentDialog];
     }
 
-    public void NextDialog()
-    {
+    public void NextDialog() {
         if (currentDialog < dialog.Count - 1)
             content.text = dialog[++currentDialog];
         else
