9e414cb20e0f411dc38a7945f5be438412bb68f8
Wuppu <wndud1640@gmail.com>
2019-11-21 17:44:04 +0900

    약간 수정

--- Assets/Script/SetResolution.cs
+++ Assets/Script/SetResolution.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Resolution : MonoBehaviour
+public class SetResolution : MonoBehaviour
 {
     private int screenSize;
     private Toggle fhd;
